a800b91c6ec54211894430cb5252d99a7a39b3f0
liam <liamdug@gmail.com>
2024-08-21 21:54:48 -0300

    Charpter 2 done

--- hecto/src/main.rs
+++ hecto/src/main.rs
@@ -1,3 +1,41 @@
+use std::io::{self, Read};
+use crossterm::terminal::enable_raw_mode;
+use crossterm::terminal::disable_raw_mode;
+
 fn main() {
-    println!("Hello, world!");
+
+    enable_raw_mode().unwrap();
+
+    for b in io::stdin().bytes() {
+        
+        match b {
+            
+            // if no error occured with b
+            Ok(b) => {
+                let c = b as char;
+
+                if c.is_control() {
+                    println!("Binary: {0:08b} ASCII: {0:#03}\r",b);
+                }
+
+                else {
+                    // {0:08b} --> toma arg cero(0:) y representa siempre con 8 bits(8) en binario(b) y rellena con ceros (0)
+                    // {0:0#3} --> toma arg cero(0:) y representa con 3 digitos, rellena con ceros.
+                    // {1:#?}  --> toma arg uno(1:) y muestra su "debug representation"
+                    println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}",b,c);
+        
+                }
+
+                if c == 'q' {
+                    break;
+                }
+            }
+
+            // if b has an error (cant be unwrapped)
+            Err(err) => println!("Error {}", err),
+        }
+    }
+
+    disable_raw_mode().unwrap();
+
 }
